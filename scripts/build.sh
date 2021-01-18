2e1c55fd80bdc2d12d29f458c398aa5bf643cae7
root <root@ns3174448.ip-51-195-62.eu>
2021-01-18 13:13:18 +0000

    basic build scripts

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+. ./env.sh
+
+mkdir -p build
+cd build
+
+#clone_and_build 1pkg_name 2repo 3commit 4cargo_args
+
+clone_and_build () {
+    rm -rf $1
+    git clone --recursive $2 $1
+    cd $1 && git checkout $3
+    cargo build $4
+}
+
+clone_and_build ton-node ${TON_NODE_GITHUB_REPO} ${TON_NODE_GITHUB_COMMIT_ID} "--release --features metrics"
+clone_and_build ton-labs-node-tools ${TON_NODE_TOOLS_GITHUB_REPO} ${TON_NODE_TOOLS_GITHUB_COMMIT_ID} "--release"
+clone_and_build tonos-cli ${TONOS_CLI_GITHUB_REPO} ${TONOS_CLI_GITHUB_COMMIT_ID} "--release"
---
+++ scripts/env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export TON_NODE_GITHUB_REPO="https://github.com/tonlabs/ton-labs-node.git"
+export TON_NODE_GITHUB_COMMIT_ID="master"
+export TON_NODE_TOOLS_GITHUB_REPO="https://github.com/tonlabs/ton-labs-node-tools.git"
+export TON_NODE_TOOLS_GITHUB_COMMIT_ID="master"
+export TONOS_CLI_GITHUB_REPO="https://github.com/tonlabs/tonos-cli.git"
+export TONOS_CLI_GITHUB_COMMIT_ID="master"
