eff1c385e070ddfa7a9bd7795d2342fbb2bed02b
root <root@ns3174448.ip-51-195-62.eu>
2021-01-21 09:37:22 +0000

    working with actions

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,8 +14,21 @@ clone_and_build () {
     git clone --recursive $2 $1
     cd $1 && git checkout $3
     cargo build $4
+    cd ..
 }
 
 clone_and_build ton-node ${TON_NODE_GITHUB_REPO} ${TON_NODE_GITHUB_COMMIT_ID} "--release --features metrics"
 clone_and_build ton-labs-node-tools ${TON_NODE_TOOLS_GITHUB_REPO} ${TON_NODE_TOOLS_GITHUB_COMMIT_ID} "--release"
 clone_and_build tonos-cli ${TONOS_CLI_GITHUB_REPO} ${TONOS_CLI_GITHUB_COMMIT_ID} "--release"
+
+if [ $1 = "--release-tar" ]; then
+    tar --transform 's/.*\///g' -cvzf rust-node-tools.tar.gz \
+        build/ton-node/target/release/ton_node \
+        build/ton-labs-node-tools/target/release/console \
+        build/ton-labs-node-tools/target/release/dhtscan \
+        build/ton-labs-node-tools/target/release/gendht \
+        build/ton-labs-node-tools/target/release/keygen \
+        build/ton-labs-node-tools/target/release/print \
+        build/ton-labs-node-tools/target/release/zerostate \
+        build/tonos-cli/target/release/tonos-cli
+fi
